7867fba7237e442c96547fe936fd43702cd45650
ivan <mazeratti48@gmail.com>
2019-10-02 22:30:46 +0200

    dynamiс array

--- battleship.js
+++ battleship.js
@@ -23,20 +23,20 @@ var model = {
 	numShips: 3,  //количество кораблей
 	shipsSunk: 0,	//количество потопленых кораблей
 	shipLength: 3,	//длина каждого корабля
-	ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""]},//позиции кораблей и координаты попаданий
-									{ locations: ["24", "34", "44"], hits: ["", "", ""]},
-									{ locations: ["10", "11", "12"], hits: ["", "", ""]}],
-	fire: function(guess) { //метод для выполнения выстрела и проверки результата
-		for (var i = 0; i < this.numShips; i++) {
-			var ship = this.ships[i];
-			var index = ship.locations.indexOf(guess);
+	ships: [{ locations: ["0", "0", "0"], hits: ["", "", ""]},//позиции кораблей и координаты попаданий
+					{ locations: ["0", "0", "0"], hits: ["", "", ""]},
+					{ locations: ["0", "0", "0"], hits: ["", "", ""]}],
+fire: function(guess) { //метод для выполнения выстрела и проверки результата
+	for (var i = 0; i < this.numShips; i++) {
+		var ship = this.ships[i];
+		var index = ship.locations.indexOf(guess);
 //если координаты клетки присутствуют в массиве locations значит выстрел попал в цель
 			if (index >= 0) {										//ставим отметку в массива hits по тому же индексу
 				ship.hits[index] = "hit"; // добавляем отметку в масив попаданий
 				view.displayHit(guess);				//Оповещаем представление о том что в клетке guess следует вывести маркер попадания
 				view.displayMessage("HIT!");
 				if (this.isSunk(ship)) { //добавим проверку здесь после того как будем точно знать что выстрел попал в корабль
-					view.displayMessage("You sank my battleship!")
+					view.displayMessage("You sank my battleship!");
 					this.shipsSunk++;
 				}
 				return true;
@@ -46,7 +46,7 @@ var model = {
 		view.displayMessage("You missed.");
 		return false;
 	},
-	isSunk: function(ship) { //метод получает объект корабля и проверяет помечены ли все его клетки маркером попадания
+isSunk: function(ship) { //метод получает объект корабля и проверяет помечены ли все его клетки маркером попадания
 		//var ship = this.ships[i];
 		for (var i = 0; i < this.shipLength; i++) {
 			if (ship.hits[i] !== "hit") {
@@ -54,7 +54,47 @@ var model = {
 			}
 		}
 		return true;
-	}
+  },
+generateShipLocations: function() {
+    var locations;
+    for (var i = 0; i < this.numShips; i++) {//набор позиций (клеток)
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));//проверка на пересикание позиций
+      this.ships[i].locations = locations;//сохранение в масиве model.ships
+    }
+  },
+generateShip: function() { //генерация позиций и направления корабля
+  var direction = Math.floor(Math.random() * 2);
+  var row, col;
+    if (direction === 1) {
+      row = Math.floor(Math.random() * this.boardSize);// начальная поз.корабля до конца конструкции
+      col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+    } else {
+      row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+      col = Math.floor(Math.random() * this.boardSize);
+    }
+    var newShipLocations = [];
+    for (var i = 0; i < this.shipLength; i++) {
+      if (direction === 1) {
+          newShipLocations.push(row + "" + (col + i));
+      } else {
+          newShipLocations.push((row + i) + "" + col);
+      }
+    }
+    return newShipLocations;
+  },
+collision: function(locations) {
+  for (var i = 0; i < this.numShips; i++) {
+    var ship = model.ships[i];
+    for (var j = 0; j < locations.length; j++) {
+      if (ship.locations.indexOf(locations[j]) >= 0) {
+        return true;
+      }
+    }
+  }
+  return false;
+  }
 };
 
 
@@ -68,8 +108,8 @@ var controller = {
 			var hit = model.fire(location);// комбинация строки и столбца передаеться методу fire
 			if (hit && model.shipsSunk === model.numShips) { 
 			//если потопленных кораблей равно количеству кораблей в игре, выводиться сообщение что все корабли потоплены
-				view.displayMessage("You sank all my battleships, in " 
-					+ this.guesses + " guesses");
+        view.displayMessage("You sank all my battleships, in " 
+          + this.guesses + " guesses");
 // выводим общее количество выстрелов, которое потребовалось игроку для того, чтобы потопить корабли
 			}
 		}
@@ -105,6 +145,7 @@ function init() {
 	fireButton.onclick = handleFireButton; //назначиние обработчика событий
 	var guessInput = document.getElementById("guessInput");
 	guessInput.onkeypress = handleKeyPress;//обработчик событий нажатия клавиш в полу ввода html
+  model.generateShipLocations();//
 }
 
 
@@ -126,4 +167,7 @@ function handleKeyPress(e) {
 
 
 
+
+
 window.onload = init;//браузер выполняет init при полной загрузке
+
